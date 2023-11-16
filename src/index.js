f483ceaedfb5e226b520c073389a27ca6a553605
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2023-11-17 00:46:48 +0500

    did some more R&D to pick panels bootstrap code best 1 and 2 are top 2 will pick 1 of them

--- src/index.js
+++ src/index.js
@@ -21,4 +21,21 @@ root.render(
 reportWebVitals();
 
 // TODO integrate coreui-free-react-admin-template-main
-//  https://coreui.io/product/free-react-admin-template/ https://coreui.io/demos/react/4.5/free/#/dashboard
+// https://github.com/refinedev/refine
+// https://refine.dev/docs/examples/
+// https://github.com/ant-design/ant-design-pro
+// https://github.com/tremorlabs/tremor
+// https://github.com/horizon-ui/horizon-ui-chakra
+// https://horizon-ui.com/horizon-ui-chakra#/admin/default
+
+// BEST
+// https://lightence-ant-design-react-template.pages.dev/
+// https://github.com/altence/lightence-ant-design-react-template
+// https://coreui.io/product/free-react-admin-template/
+// https://coreui.io/demos/react/4.5/free/#/dashboard
+// https://demo.themesberg.com/volt-react-dashboard/#/     (VERSION 16)
+// https://github.com/themesberg/volt-react-dashboard
+// https://github.com/creativetimofficial/material-dashboard-react
+// https://mantisdashboard.io/
+// https://www.creative-tim.com/product/muse-ant-design-dashboard
+// https://www.creative-tim.com/product/argon-dashboard-chakra
